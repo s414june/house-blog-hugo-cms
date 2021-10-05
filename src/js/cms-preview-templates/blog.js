19b696984c80aa74fc69f2a9a5f41aee9acfb957
YU CHIN WU <s414june@gmail.com>
2021-10-05 12:53:09 +0800

    m

--- src/js/cms-preview-templates/blog.js
+++ src/js/cms-preview-templates/blog.js
@@ -7,25 +7,12 @@ export default class PostPreview extends React.Component {
             let image = getAsset(entry.getIn(["data", "image"]));
 
             return <div className = "mw6 center ph3 pv4" >
-                <
-                h1 className = "f2 lh-title b mb3" > { entry.getIn(["data", "title"]) } < /h1> <
-            div className = "flex justify-between grey-3" >
-                <
-                p > { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") } < /p> { /* <p > Read in x minutes < /p>  */ } <
-                /div> {
-                /* <div className="cms mw6">
+                <h1 className = "f2 lh-title b mb3" > { entry.getIn(["data", "title"]) } </h1> <div className = "flex justify-between grey-3" >
+                <p> { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") } </p> { /* <p > Read in x minutes < /p>  */ } </div>
+                <div className="cms mw6">
                         <p>{ entry.getIn(["data", "description"]) }</p>
                         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
                         { widgetFor("body") }
-                      </div> */
-        } <
-        div className = "" >
-        <
-        p > { entry.getIn(["data", "description"]) } < /p> {
-    image && < img src = { image }
-    alt = { entry.getIn(["data", "title"]) }
-    /> } { widgetFor("body") } < /
-    div > <
-        /div>;
+                      </div>  </div>;
 }
 }
