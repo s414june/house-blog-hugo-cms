b6a15c14c401b49fe51667f6ee442980f4f32fed
YU CHIN WU <s414june@gmail.com>
2021-10-05 17:46:01 +0800

    update

--- src/js/cms-preview-templates/blog.js
+++ src/js/cms-preview-templates/blog.js
@@ -7,12 +7,11 @@ export default class PostPreview extends React.Component {
             let image = getAsset(entry.getIn(["data", "image"]));
 
             return <div className = "mw6 center ph3 pv4" >
-                <h1 className = "f2 lh-title b mb3" > { entry.getIn(["data", "title"]) } </h1> <div className = "flex justify-between grey-3" >
-                <ul class="list-inline d-flex justify-content-between py-3">
-                  <li class="list-inline-item">發文者： 邱建奕</li>
-                  <li class="list-inline-item">{ format(entry.getIn(["data", "date"]), "YYYY/MMM/D") }</li>
-                </ul>
-                <p> { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") } </p> { /* <p > Read in x minutes < /p>  */ } </div>
+                <h1 className = "f2 lh-title b mb3" > { entry.getIn(["data", "title"]) } </h1>
+                <div className = "flex justify-between grey-3" >
+                  <p>發文者： 邱建奕</p>
+                  <p> { format(entry.getIn(["data", "date"]), "YYYY / MM / D") } </p> { /* <p > Read in x minutes < /p>  */ }
+                </div>
                 <div className="cms mw6">
                         {/* <p>{ entry.getIn(["data", "description"]) }</p> */}
                         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
