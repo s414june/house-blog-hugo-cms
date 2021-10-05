7db0ff5bafae21495c5036920067585c26d88889
YU CHIN WU <s414june@gmail.com>
2021-10-05 11:53:38 +0800

    update

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,7 +3,6 @@ import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
-import parsaStyles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/parsa.css";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
@@ -13,7 +12,6 @@ import ContactPreview from "./cms-preview-templates/contact";
 import BlogPreview from "./cms-preview-templates/blog";
 
 CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewStyle(parsaStyles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
